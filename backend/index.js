b2c49ae75b8fcaf645e510675fc1bf0c1a4c9e8c
EbtehalAbuhassan <abuhassanebtehal@gmail.com>
2023-04-06 21:53:49 +0300

    infouser

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
+require('dotenv').config()
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -8,6 +8,52 @@ app.use(cors());
 app.use(express.json());
 
 // Handles any other endpoints [unassigned - endpoints]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const infouserRouter = require("./routes/infouser")
+
+
+app.use("/infouser",infouserRouter )
+
+
+
+
+
+
+
+
+
+
+
+
 app.use("*", (req, res) => res.status(404).json("NO content at this path"));
 
 app.listen(PORT, () => {
