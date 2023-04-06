6361158f99983bf21a06e43f1d26152cc7bb2f90
BatoolTarawneh <batoolatarawneh@gmail.com>
2023-04-06 21:49:02 +0300

    middleware

---
+++ backend/middleware/authorization.js
@@ -0,0 +1,22 @@
+const pool = require("../models/db")
+
+const authorization = (string) =>{
+    return function (req,res,next){
+        const role_id = req.token.role;
+        const data =[role_id,string];
+        const query = `SELECT * FROM role_permission  INNER JOIN permissions  ON role_permission.permission_id = permissions.id WHERE role_permission.role_id = ($1) AND permissions.permission = ($2)`
+         pool.query(query, data).then((result)=>{
+            if(result.rows.length){
+                next();
+            }
+            else{
+                throw Error;
+            }
+         })
+         .catch((error)=>{request.status(400).json({error:error,message: "unauthorized"})
+
+    })
+}
+}
+
+module.exports = authorization;
